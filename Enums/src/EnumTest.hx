c1586b480a2d7989d4b4f0aa5babd53cf3585cce
Grabli66 <grabli66@gmail.com>
2019-03-30 20:00:28 +0300

    Enum test

--- Enums/src/EnumTest.hx
+++ Enums/src/EnumTest.hx
@@ -3,14 +3,21 @@ enum MyValue {
 	Int(v:Int);
 	Float(v:Float);
 	Some(v:Int, s:String, arg:Array<Int>);
+	Rec(v:MyValue);
 }
 
 class EnumTest {
 	public static function main() {
-		var value = MyValue.Int(33);
-		trace(value);		
+		var value1 = MyValue.Int(33);
+		trace(value1);		
 
-		var value = MyValue.Float(33);
-		trace(value);		
+		var value2 = MyValue.Float(33);
+		trace(value2);
+
+		var value3 = MyValue.Int(33);
+		trace(value1 == value3);
+
+		var arr = [Int(3), Float(4), None, Some(1, "Good", [1,2,3])];
+		trace(arr);
 	}
 }
