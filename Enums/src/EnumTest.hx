9fa7b0175adba4f4d650ab79928c24e346b26d5e
Grabli66 <grabli66@gmail.com>
2019-03-31 10:56:19 +0300

    Fixes

--- Enums/src/EnumTest.hx
+++ Enums/src/EnumTest.hx
@@ -8,16 +8,23 @@ enum MyValue {
 
 class EnumTest {
 	public static function main() {
-		var value1 = MyValue.Int(33);
-		trace(value1);		
+		// var value1 = MyValue.Int(33);
+		// trace(value1);		
 
-		var value2 = MyValue.Float(33);
-		trace(value2);
+		// var value2 = MyValue.Float(33);
+		// trace(value2);
 
-		var value3 = MyValue.Int(33);
-		trace(value1 == value3);
+		// var value3 = MyValue.Int(33);
+		// trace(value1 == value3);
 
-		var arr = [Int(3), Float(4), None, Some(1, "Good", [1,2,3])];
-		trace(arr);
+		// var arr = [Int(3), Float(4), None, Some(1, "Good", [1,2,3])];
+		// trace(arr);
+
+		var res = new Array<MyValue>();
+		for (i in 0...1000000) {
+			res.push(Rec(Float(33)));
+		}
+
+		trace(res.length);		
 	}
 }
