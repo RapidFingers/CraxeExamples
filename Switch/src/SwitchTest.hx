13f049a794f493814e67bbc8180609143c6857f5
Grabli66 <grabli66@gmail.com>
2019-04-05 17:02:17 +0300

    Fix switch for enums

--- Switch/src/SwitchTest.hx
+++ Switch/src/SwitchTest.hx
@@ -16,15 +16,22 @@ enum DataType {
 class SwitchTest {
 	public static function main() {
 		// Test ADT switch
-		var s = TMyData(new MyData("Hello!"));
-		switch (s) {
-			case TInteger(v):
-			case TDouble(v):
-				trace(v);
-			case TString(v):
-			case TMyData(v):
-				trace(v.data);
-		}		
+
+		var enums = new Array<DataType>();
+		enums.push(TInteger(1));
+		enums.push(TMyData(new MyData("Hello!")));
+
+		for (s in enums) {
+			switch (s) {
+				case TInteger(v):
+					trace(v);
+				case TDouble(v):
+					trace(v);
+				case TString(v):
+				case TMyData(v):
+					trace(v.data);
+			}
+		}
 
 		// Test Int switch
 		var s = 33;
