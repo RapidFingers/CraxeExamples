61710d480d22e329584b4f5ba4a51f26705e6747
Grabli66 <grabli66@gmail.com>
2019-04-17 22:13:32 +0300

    Typedefs magic

--- Typedef/src/TypedefTest.hx
+++ Typedef/src/TypedefTest.hx
@@ -4,40 +4,7 @@ typedef User = {
 	var email:String;
 }
 
-typedef OtherUser = {
-	var id:Int;
-	var name:String;
-}
-
-typedef Admin = User & {
-	level:Int
-}
-
-typedef SomeProc = () -> String;
-typedef SomeInt = Int;
-typedef SomeUser = ClassUser;
-
-class ClassUser {
-	public var id:Int;
-	public var name:String;
-	public var email:String;
-
-	public function new(id:Int, name:String, email:String) {
-		this.id = id;
-		this.name = name;
-		this.email = email;
-	}
-}
-
 class TypedefTest {
-	static function getUser() {
-		return {
-			id:44,
-			name:"Superman",
-			email:"superman@gmail.com"
-		}
-	}
-
 	public static function main() {
 		var admin:User = {
 			id: 33,
@@ -45,18 +12,13 @@ class TypedefTest {
 			email: "batman@batman.com"			
 		}
 
-		var arr = new Array<{id:Int, name:String, email:String}>();
+		var arr = new Array<User>();
 		arr.push(admin);
 
-		var user = getUser();
-		arr.push(user);
-
-		var clsUser = new ClassUser(44, "Superman", "super@gmail.com");
-		arr.push(clsUser);
-
-		var obj = arr[0];
-		trace(obj.email);
-		obj.email = "shit";
-		trace(user.email);
+		arr.push({
+			id: 11,
+			name: "GOOD",
+			email: "good"
+		});
 	}
 }
