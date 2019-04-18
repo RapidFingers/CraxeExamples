95ae579dd650d767ffa9f5c7b8748f78b1638475
Grabli66 <grabli66@gmail.com>
2019-04-18 17:00:34 +0300

    Almost working typedefs

--- Typedef/src/TypedefTest.hx
+++ Typedef/src/TypedefTest.hx
@@ -4,23 +4,44 @@ typedef User = {
 	var email:String;
 }
 
+class ClassUser {
+	public var id:Int;
+	public var name:String;
+	public var email:String;
+
+	public function new(id:Int, name:String, email:String) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+}
+
 class TypedefTest {
+	static function test(arr:Array<User>) {
+		for (it in arr) {
+			trace(it.name);
+		}
+	}
+
 	public static function main() {
 		var admin:User = {
 			id: 33,
 			name: "Batman",
-			email: "batman@batman.com"			
+			email: "batman@batman.com"
 		}
 
-		var arr = new Array<User>();
+		var arr = new Array<{id:Int, name:String, email:String}>();
 		arr.push(admin);
 
+		var clsUser = new ClassUser(44, "Superman", "super@gmail.com");
+		arr.push(clsUser);
+		
 		arr.push({
 			id: 11,
 			name: "GOOD",
 			email: "good"
 		});
 
-		trace(arr);
+		test(arr);
 	}
 }
