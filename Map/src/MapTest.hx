143386d7af7db6e8fd580d884ddc9ad42385d16d
Grabli66 <grabli66@gmail.com>
2019-04-24 16:27:43 +0300

    Maps example

---
+++ Map/src/MapTest.hx
@@ -0,0 +1,30 @@
+class SomeKey {
+	public var key:String;
+
+	public function new(key:String) {
+		this.key = key;
+	}	
+}
+
+class MapTest {
+	public static function main() {
+		var stringmap = new Map<String, String>();
+		stringmap["batman"] = "batman@gmail.com";
+		stringmap["superman"] = "superman@gmail.com";
+		trace(stringmap);
+		trace(stringmap["batman"]);
+
+
+		var intmap = new Map<Int, String>();
+		intmap[33] = "batman";
+		intmap[44] = "superman";
+		trace(intmap);
+
+		var objmap = new Map<SomeKey, String>();
+		objmap[new SomeKey("batman")] = "batman@gmail.com";
+		objmap[new SomeKey("superman")] = "superman@gmail.com";
+		trace(objmap);
+		// var eq = objmap[new SomeKey("batman")] == objmap[new SomeKey("batman")];
+		// trace(eq);
+	}
+}
