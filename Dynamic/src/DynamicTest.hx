681505a1b00ba9bad970db03778d45aa30376d4f
Grabli66 <grabli66@gmail.com>
2019-05-08 15:05:28 +0300

    Work on dynamic

--- Dynamic/src/DynamicTest.hx
+++ Dynamic/src/DynamicTest.hx
@@ -1,4 +1,22 @@
+class User {
+	public var id:String;
+
+	public function new() {
+		this.id = "UserID";
+	}
+}
+
 class DynamicTest {
+	static function getDynamic():Dynamic  {
+		return "Hello";
+	}
+
+	static function passDynamic(d:String) {
+		trace(d);
+	}
+
 	public static function main() {	
+		var d = getDynamic();
+		passDynamic(d);
 	}
 }
